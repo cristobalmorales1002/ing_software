cb1c803f6a0e88b91975972ccc3e49ad440507e1
Cristóbal <cristobal.morales2201@alumnos.ubiobio.cl>
2025-10-23 18:37:07 -0300

    Registro

--- src/main/java/com/ingsoftware/proyectosemestral/Modelo/Pregunta.java
+++ src/main/java/com/ingsoftware/proyectosemestral/Modelo/Pregunta.java
@@ -1,97 +1,36 @@
 package com.ingsoftware.proyectosemestral.Modelo;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Pregunta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long pregunta_id;
+    @Column(nullable = false)
     private int orden;
+    @Column(nullable = false)
     private TipoDato tipo_dato;
+    @Column(nullable = false)
     private String etiqueta;
+    @Column(nullable = false)
     private boolean dato_sensible;
+    @Column(nullable = false)
     private boolean activo;
+    @Column(nullable = false)
     private String descripcion;
+    @Column(nullable = false)
     private double dicotomizacion;
+    @Column(nullable = false)
     private SentidoCorte sentido_corte;
-
-    public Pregunta(Long pregunta_id, int orden, TipoDato tipo_dato, String etiqueta, boolean dato_sensible, boolean activo, String descripcion, double dicotomizacion, SentidoCorte sentido_corte) {
-        this.pregunta_id = pregunta_id;
-        this.orden = orden;
-        this.tipo_dato = tipo_dato;
-        this.etiqueta = etiqueta;
-        this.dato_sensible = dato_sensible;
-        this.activo = activo;
-        this.descripcion = descripcion;
-        this.dicotomizacion = dicotomizacion;
-        this.sentido_corte = sentido_corte;
-    }
-
-    public Long getPregunta_id() {
-        return pregunta_id;
-    }
-
-    public void setPregunta_id(Long pregunta_id) {
-        this.pregunta_id = pregunta_id;
-    }
-
-    public int getOrden() {
-        return orden;
-    }
-
-    public void setOrden(int orden) {
-        this.orden = orden;
-    }
-
-    public TipoDato getTipo_dato() {
-        return tipo_dato;
-    }
-
-    public void setTipo_dato(TipoDato tipo_dato) {
-        this.tipo_dato = tipo_dato;
-    }
-
-    public String getEtiqueta() {
-        return etiqueta;
-    }
-
-    public void setEtiqueta(String etiqueta) {
-        this.etiqueta = etiqueta;
-    }
-
-    public boolean isDato_sensible() {
-        return dato_sensible;
-    }
-
-    public void setDato_sensible(boolean dato_sensible) {
-        this.dato_sensible = dato_sensible;
-    }
-
-    public boolean isActivo() {
-        return activo;
-    }
-
-    public void setActivo(boolean activo) {
-        this.activo = activo;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public double getDicotomizacion() {
-        return dicotomizacion;
-    }
-
-    public void setDicotomizacion(double dicotomizacion) {
-        this.dicotomizacion = dicotomizacion;
-    }
-
-    public SentidoCorte getSentido_corte() {
-        return sentido_corte;
-    }
-
-    public void setSentido_corte(SentidoCorte sentido_corte) {
-        this.sentido_corte = sentido_corte;
-    }
 }
--- src/main/java/com/ingsoftware/proyectosemestral/Modelo/Registro.java
+++ src/main/java/com/ingsoftware/proyectosemestral/Modelo/Registro.java
@@ -1,6 +1,27 @@
 package com.ingsoftware.proyectosemestral.Modelo;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Registro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long registro_id;
+    @Column(nullable = false)
     private String accion;
+    @Column(nullable = false)
+    private String detalles;
+    @Column(nullable = false)
+    private LocalDateTime registro_fecha;
 }

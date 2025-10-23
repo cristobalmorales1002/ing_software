ba20a368146ca7badcdd8099d031895f5497e92f
matias <matias.aguilera2101@alumnos.ubiobio.cl>
2025-10-23 18:31:03 -0300

    permiso creado

--- src/main/java/com/ingsoftware/proyectosemestral/Modelo/Permiso.java
+++ src/main/java/com/ingsoftware/proyectosemestral/Modelo/Permiso.java
@@ -1,4 +1,24 @@
 package com.ingsoftware.proyectosemestral.Modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Permiso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id_perm;
+    String nombre;
+    String descripcion;
 }

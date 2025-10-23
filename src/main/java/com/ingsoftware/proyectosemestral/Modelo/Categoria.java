d29ef1edc0ff2183a53a100b517d167fb4ce2a26
Bairon <bairon.munoz2201@alumnos.ubiobio.cl>
2025-10-23 18:38:08 -0300

    Implementación clase OpcionPregunta

--- src/main/java/com/ingsoftware/proyectosemestral/Modelo/Categoria.java
+++ src/main/java/com/ingsoftware/proyectosemestral/Modelo/Categoria.java
@@ -1,9 +1,6 @@
 package com.ingsoftware.proyectosemestral.Modelo;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,7 +16,11 @@ public class Categoria {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id_cat;
+
+    @Column(nullable = false)
     private String nombre;
+
+    @Column(nullable = false)
     private int orden;
 
 }

1e138dac8c9518758084f7883617728acbf0541e
Cristóbal <cristobal.morales2201@alumnos.ubiobio.cl>
2025-10-23 18:42:43 -0300

    Respuesta

--- src/main/java/com/ingsoftware/proyectosemestral/Modelo/Respuesta.java
+++ src/main/java/com/ingsoftware/proyectosemestral/Modelo/Respuesta.java
@@ -1,4 +1,21 @@
 package com.ingsoftware.proyectosemestral.Modelo;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Respuesta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long respuesta_id;
+    @Column(nullable = false)
+    private String valor;
 }

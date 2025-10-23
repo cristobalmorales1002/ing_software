430905d8c13bd0f947fcfbe72f86c3e06939cc17
matias <matias.aguilera2101@alumnos.ubiobio.cl>
2025-10-23 18:42:17 -0300

    permiso y paciente arreglado

--- src/main/java/com/ingsoftware/proyectosemestral/Modelo/Paciente.java
+++ src/main/java/com/ingsoftware/proyectosemestral/Modelo/Paciente.java
@@ -1,59 +1,28 @@
 package com.ingsoftware.proyectosemestral.Modelo;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Paciente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long participante_id;
-    String participante_cod;
-    Boolean esCaso;
-    LocalDate fecha_incl;
+    private Long participante_id;
+    @Column(nullable = false)
+    private String participante_cod;
+    @Column(nullable = false)
+    private Boolean esCaso;
+    @Column(nullable = false)
+    private LocalDate fecha_incl;
 
-    public Paciente(Long participante_id, String participante_cod, Boolean esCaso, LocalDate fecha_incl) {
-        this.participante_id = participante_id;
-        this.participante_cod = participante_cod;
-        this.esCaso = esCaso;
-        this.fecha_incl = fecha_incl;
-    }
-
-    public Paciente() {}
-
-    public Long getParticipante_id() {
-        return participante_id;
-    }
-
-    public void setParticipante_id(Long participante_id) {
-        this.participante_id = participante_id;
-    }
-
-    public String getParticipante_cod() {
-        return participante_cod;
-    }
-
-    public void setParticipante_cod(String participante_cod) {
-        this.participante_cod = participante_cod;
-    }
-
-    public Boolean getEsCaso() {
-        return esCaso;
-    }
-
-    public void setEsCaso(Boolean esCaso) {
-        this.esCaso = esCaso;
-    }
-
-    public LocalDate getFecha_incl() {
-        return fecha_incl;
-    }
-
-    public void setFecha_incl(LocalDate fecha_incl) {
-        this.fecha_incl = fecha_incl;
-    }
 }

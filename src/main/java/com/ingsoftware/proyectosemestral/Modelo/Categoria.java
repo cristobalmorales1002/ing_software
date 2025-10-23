a3438381013b1ddb867d2b94e39137367f07e7f3
Bairon <bairon.munoz2201@alumnos.ubiobio.cl>
2025-10-23 18:30:37 -0300

    Implementación clase Categoria

--- src/main/java/com/ingsoftware/proyectosemestral/Modelo/Categoria.java
+++ src/main/java/com/ingsoftware/proyectosemestral/Modelo/Categoria.java
@@ -1,4 +1,25 @@
 package com.ingsoftware.proyectosemestral.Modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Categoria {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id_cat;
+    private String nombre;
+    private int orden;
+
 }

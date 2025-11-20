1a4570d390233e260733e50fc060effda98c9259
matias <matias.aguilera2101@alumnos.ubiobio.cl>
2025-11-20 12:22:35 -0300

    Iniciar con EstadisticaServicio

--- src/main/java/com/ingsoftware/proyectosemestral/Servicio/EstadisticaServicio.java
+++ src/main/java/com/ingsoftware/proyectosemestral/Servicio/EstadisticaServicio.java
@@ -1,5 +1,30 @@
 package com.ingsoftware.proyectosemestral.Servicio;
 
+import com.ingsoftware.proyectosemestral.DTO.EstadisticaDemograficaDto;
+import com.ingsoftware.proyectosemestral.DTO.EstadisticaDto;
+import com.ingsoftware.proyectosemestral.Modelo.Pregunta;
+import com.ingsoftware.proyectosemestral.Modelo.Respuesta;
+import com.ingsoftware.proyectosemestral.Repositorio.PreguntaRepositorio;
+import com.ingsoftware.proyectosemestral.Repositorio.RespuestaRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
 public class EstadisticaServicio {
+    @Autowired
+    private RespuestaRepositorio respuestaRepositorio;
+
+    @Autowired
+    private PreguntaRepositorio preguntaRepositorio;
+
+    private static final String ETIQUETA_SEXO = "SEXO";
+    private static final String ETIQUETA_FECHA_NACIMIENTO = "FECHA_NACIMIENTO";
 }

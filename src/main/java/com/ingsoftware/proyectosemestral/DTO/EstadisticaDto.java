e0994633fd6c3dbde32980b749061b6ef34ca6b4
matias <matias.aguilera2101@alumnos.ubiobio.cl>
2025-11-20 12:01:31 -0300

    Realizar la clase EstadisticaDto

--- src/main/java/com/ingsoftware/proyectosemestral/DTO/EstadisticaDto.java
+++ src/main/java/com/ingsoftware/proyectosemestral/DTO/EstadisticaDto.java
@@ -1,4 +1,12 @@
 package com.ingsoftware.proyectosemestral.DTO;
 
+import lombok.Builder;
+import lombok.Data;
+import java.util.Map;
+
+@Data
+@Builder
 public class EstadisticaDto {
+    private String nombreEstadistica;
+    private Map<String, Long> conteoPorCategoria;
 }

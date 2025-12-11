91811bb2289633adb2876530e84b9c36bf7dea30
matias <matias.aguilera2101@alumnos.ubiobio.cl>
2025-12-11 20:04:23 -0300

    Comence agregarle cosas a CondicionServicio

--- backend/src/main/java/com/ingsoftware/proyectosemestral/Servicio/CondicionServicio.java
+++ backend/src/main/java/com/ingsoftware/proyectosemestral/Servicio/CondicionServicio.java
@@ -1,4 +1,23 @@
 package com.ingsoftware.proyectosemestral.Servicio;
 
+import com.ingsoftware.proyectosemestral.Modelo.Pregunta;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
 public class CondicionServicio {
+
+    public String evaluarEstadoPregunta(Pregunta pregunta, Map<Long, String> respuestasActuales) {
+        if (pregunta.getPreguntaControladora() == null) {
+            return "VISIBLE";
+        }
+
+        Long idControladora = pregunta.getPreguntaControladora().getPregunta_id();
+        String valorEsperado = pregunta.getValorEsperadoControladora();
+        String accion = pregunta.getAccionSiNoCumple();
+
+        String valorActual = respuestasActuales.get(idControladora);
+        return "VISIBLE";
+    }
 }
